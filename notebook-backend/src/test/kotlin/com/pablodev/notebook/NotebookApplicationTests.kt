13958989f54ec084a1d38cdbbc5ebf6b45ae90c5
Pablodev <pablomartinezcoder@gmail.com>
2025-05-11 13:43:34 +0200

    Initial commit

---
+++ notebook-backend/src/test/kotlin/com/pablodev/notebook/NotebookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pablodev.notebook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
