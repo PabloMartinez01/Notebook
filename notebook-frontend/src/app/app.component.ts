81d60992512049ce88352736f1205ed261569277
Pablodev <pablomartinezcoder@gmail.com>
2025-05-15 12:05:19 +0200

    Feat: Add markdown viewer

--- notebook-frontend/src/app/app.component.ts
+++ notebook-frontend/src/app/app.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import {MarkdownComponent} from 'ngx-markdown';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    MarkdownComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'notebook-frontend';
+  noteMarkdown: string = `
+# Test Note
+
+This is a short **Markdown** example.
+
+## List
+
+- First item
+- Second item
+- **Important** item
+
+## Table
+
+| Name  | Age |
+|-------|-----|
+| John  | 25  |
+| Mary  | 30  |
+
+## JavaScript Code
+
+\`\`\`js
+function greet(name) {
+  console.log('Hello, ' + name + '!');
+}
+
+greet('World');
+\`\`\`
+
+End of the note. 🎉
+`;
 }
