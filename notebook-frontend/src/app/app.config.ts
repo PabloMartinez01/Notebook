3bfa6c4eb3635653625050278c4390aa8d12a87c
Pablodev <pablomartinezcoder@gmail.com>
2025-05-15 16:54:21 +0200

    Feat: Add custom preset

--- notebook-frontend/src/app/app.config.ts
+++ notebook-frontend/src/app/app.config.ts
@@ -6,6 +6,25 @@ import {provideMarkdown} from 'ngx-markdown';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import {definePreset} from '@primeng/themes';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{blue.50}',
+      100: '{blue.100}',
+      200: '{blue.200}',
+      300: '{blue.300}',
+      400: '{blue.400}',
+      500: '{blue.500}',
+      600: '{blue.600}',
+      700: '{blue.700}',
+      800: '{blue.800}',
+      900: '{blue.900}',
+      950: '{blue.950}'
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,9 +34,11 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: MyPreset
       }
     })
 
   ]
 };
+
+
