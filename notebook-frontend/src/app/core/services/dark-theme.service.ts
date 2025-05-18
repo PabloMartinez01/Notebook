8d0161d5060c4b579b327d1e81d007884b988096
Pablodev <pablomartinezcoder@gmail.com>
2025-05-18 11:00:49 +0200

    Feat: Add dark theme observable

--- notebook-frontend/src/app/core/services/dark-theme.service.ts
+++ notebook-frontend/src/app/core/services/dark-theme.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DarkThemeService {
 
-  constructor() { }
+    private darkThemeSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.getSystemTheme());
+    isDarkTheme$ = this.darkThemeSubject.asObservable();
+
+    constructor() {
+      const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+      mediaQuery.addEventListener('change', (e) => {
+        this.setDarkMode(e.matches);
+      });
+    }
+
+    private getSystemTheme(): boolean {
+      return window.matchMedia('(prefers-color-scheme: dark)').matches;
+    }
+
+    private setDarkMode(isDark: boolean) {
+      this.darkThemeSubject.next(isDark);
+    }
+
+    get isDarkMode(): boolean {
+      return this.darkThemeSubject.value;
+    }
 }
