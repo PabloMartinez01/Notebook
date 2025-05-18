a2b49bd5af33f30be83ce1e5533ad03249de33f7
Pablodev <pablomartinezcoder@gmail.com>
2025-05-18 10:34:47 +0200

    Refactor: Remove unused imports

--- notebook-frontend/src/app/app.component.ts
+++ notebook-frontend/src/app/app.component.ts
@@ -1,19 +1,7 @@
 import { Component } from '@angular/core';
-import {MarkdownComponent} from 'ngx-markdown';
-import {Textarea} from 'primeng/textarea';
 import {FormsModule} from '@angular/forms';
-import {EditorComponent} from 'ngx-monaco-editor-v2';
 import {Splitter} from 'primeng/splitter';
-import {Menubar} from 'primeng/menubar';
-import {MenuItem, MenuItemCommandEvent} from 'primeng/api';
-import {Sidebar} from 'primeng/sidebar';
-import {Button} from 'primeng/button';
-import {Drawer} from 'primeng/drawer';
 import {NOTE_MARKDOWN} from './core/app.constants';
-import {editor} from 'monaco-editor';
-import IEditorOptions = editor.IEditorOptions;
-import EditorOptionsType = editor.EditorOptionsType;
-import EditorOption = editor.EditorOption;
 import {SidebarComponent} from './shared/sidebar/sidebar.component';
 import {CodeEditorComponent} from './features/note/components/editor/code-editor.component';
 import {ViewerComponent} from './features/note/components/viewer/viewer.component';
@@ -22,14 +10,8 @@ import {MenubarComponent} from './features/note/components/toolbar/menubar.compo
 @Component({
   selector: 'app-root',
   imports: [
-    MarkdownComponent,
     FormsModule,
-    EditorComponent,
     Splitter,
-    Menubar,
-    Sidebar,
-    Button,
-    Drawer,
     SidebarComponent,
     CodeEditorComponent,
     ViewerComponent,
