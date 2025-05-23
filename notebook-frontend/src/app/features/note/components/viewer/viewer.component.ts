ba98f0a0471a83ef66c87fde6315f66b1d15ff7b
Pablodev <pablomartinezcoder@gmail.com>
2025-05-23 15:00:06 +0200

    Feat: Viewer change between dark and light themes

--- notebook-frontend/src/app/features/note/components/viewer/viewer.component.ts
+++ notebook-frontend/src/app/features/note/components/viewer/viewer.component.ts
@@ -1,15 +1,30 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {MarkdownComponent} from 'ngx-markdown';
+import {DarkThemeService} from '../../../../core/services/dark-theme.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'viewer',
   imports: [
-    MarkdownComponent
+    MarkdownComponent,
+    NgClass
   ],
   templateUrl: './viewer.component.html'
 })
-export class ViewerComponent {
+export class ViewerComponent implements OnInit{
 
   @Input() noteMarkdown: string = '';
+  darkMode: boolean = false;
+
+  constructor(private darkThemeService: DarkThemeService) {
+
+  }
+
+  ngOnInit(): void {
+    this.darkThemeService.isDarkTheme$.subscribe({
+      next: (darkMode: boolean) => this.darkMode = darkMode,
+      error: err => console.log(err)
+    })
+  }
 
 }
