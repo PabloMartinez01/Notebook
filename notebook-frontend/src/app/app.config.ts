5b44627e7e9f646e300d463c9989e9c56f0d4d37
Pablodev <pablomartinezcoder@gmail.com>
2025-05-30 15:54:04 +0200

    Refactor: Extracted preset configuration files

--- notebook-frontend/src/app/app.config.ts
+++ notebook-frontend/src/app/app.config.ts
@@ -1,38 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {provideMarkdown} from 'ngx-markdown';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-import {definePreset} from '@primeng/themes';
 import {NgxMonacoEditorConfig, provideMonacoEditor} from 'ngx-monaco-editor-v2';
 import * as monaco from 'monaco-editor';
-import {editor} from 'monaco-editor';
-import IStandaloneThemeData = editor.IStandaloneThemeData;
-import IStandaloneEditorConstructionOptions = editor.IStandaloneEditorConstructionOptions;
 import {customDarkTheme, customLightTheme} from './core/configuration/monaco-themes';
-
-const MyPreset = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: '{blue.50}',
-      100: '{blue.100}',
-      200: '{blue.200}',
-      300: '{blue.300}',
-      400: '{blue.400}',
-      500: '{blue.500}',
-      600: '{blue.600}',
-      700: '{blue.700}',
-      800: '{blue.800}',
-      900: '{blue.900}',
-      950: '{blue.950}'
-    }
-  }
-});
-
-
+import {applicationPreset} from './core/configuration/primeng-preset';
 
 
 const monacoConfig: NgxMonacoEditorConfig = {
@@ -55,7 +31,7 @@ export const appConfig: ApplicationConfig = {
         options: {
           darkModeSelector: '.app-dark'
         },
-        preset: MyPreset
+        preset: applicationPreset
       }
     }),
     provideMonacoEditor(monacoConfig) ,
