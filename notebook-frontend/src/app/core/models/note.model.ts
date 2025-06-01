4c8733f3fa12ddbedd5fe87de697cf58260db4d5
Pablodev <pablomartinezcoder@gmail.com>
2025-06-01 19:06:00 +0200

    Feat: Add find all notes method

---
+++ notebook-frontend/src/app/core/models/note.model.ts
@@ -0,0 +1,5 @@
+export interface NoteResponse {
+    id: string;
+    title: string;
+    content: string;
+}
--- notebook-frontend/src/app/core/services/note-service.service.ts
+++ notebook-frontend/src/app/core/services/note-service.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {NoteResponse} from '../models/note.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,10 @@ export class NoteServiceService {
   constructor(private httpClient: HttpClient) {
 
   }
+
+  findAllNotes(): Observable<NoteResponse[]> {
+    return this.httpClient.get<NoteResponse[]>("http://localhost:8080/notes")
+  }
+
 
 }
