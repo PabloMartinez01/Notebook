5daa4b8e8cbedf7b4309f32f975fbee744d54c6e
Pablodev <pablomartinezcoder@gmail.com>
2025-06-04 18:24:15 +0200

    Feat: Add cors filter bean

---
+++ notebook-backend/src/main/kotlin/com/pablodev/notebook/configuration/SecurityConfiguration.kt
@@ -0,0 +1,27 @@
+package com.pablodev.notebook.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class SecurityConfiguration {
+
+    @Bean
+    fun corsConfiguration(): CorsFilter {
+        val corsConfiguration: CorsConfiguration = CorsConfiguration()
+        corsConfiguration.allowedHeaders = listOf("*")
+        corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        corsConfiguration.allowedOriginPatterns = listOf("*")
+        corsConfiguration.allowCredentials = true
+
+        val urlBasedCorsConfigurationSource = UrlBasedCorsConfigurationSource()
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration)
+
+        return CorsFilter(urlBasedCorsConfigurationSource)
+    }
+
+}
