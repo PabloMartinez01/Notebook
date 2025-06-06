39549ae40cfa03b7a57164e8538bd98de6a184a7
Pablodev <pablomartinezcoder@gmail.com>
2025-06-06 17:08:30 +0200

    Feat: Add welcome component

---
+++ notebook-frontend/src/app/features/welcome/pages/welcome-view/welcome-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-view',
+  imports: [],
+  templateUrl: './welcome-view.component.html',
+  styleUrl: './welcome-view.component.css'
+})
+export class WelcomeViewComponent {
+
+}
