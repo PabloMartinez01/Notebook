d53977e30a5359c8ee4e14b8e45f93d33e6c2fa7
Pablodev <pablomartinezcoder@gmail.com>
2025-06-06 17:00:56 +0200

    Feat: Add note component

---
+++ notebook-frontend/src/app/features/note/pages/view-note/view-note.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-note',
+  imports: [],
+  templateUrl: './view-note.component.html',
+  styleUrl: './view-note.component.css'
+})
+export class ViewNoteComponent {
+
+}
