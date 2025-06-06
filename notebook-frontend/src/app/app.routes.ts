62ad9d987f43e8aa88e81fdc76b0325695c444f8
Pablodev <pablomartinezcoder@gmail.com>
2025-06-06 17:02:00 +0200

    Feat: Add note route

--- notebook-frontend/src/app/app.routes.ts
+++ notebook-frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import {AppComponent} from './app.component';
+import {ViewNoteComponent} from './features/note/pages/view-note/view-note.component';
 
 export const routes: Routes = [
   {
     path: 'note/:id',
-    component: AppComponent
+    component: ViewNoteComponent
   }
 ];
