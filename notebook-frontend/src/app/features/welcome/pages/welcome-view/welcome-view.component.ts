79c3dc951bdd6e2f8dd3128edcc364be3fff865e
unknown <pablomartinezcoder@gmail.com>
2025-06-21 11:03:45 +0200

    Feat: Add emoji library

--- notebook-frontend/src/app/features/welcome/pages/welcome-view/welcome-view.component.ts
+++ notebook-frontend/src/app/features/welcome/pages/welcome-view/welcome-view.component.ts
@@ -1,13 +1,15 @@
 import {Component} from '@angular/core';
+import {PickerComponent} from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-welcome-view',
   imports: [
+    PickerComponent
 
   ],
   templateUrl: './welcome-view.component.html'
 })
 export class WelcomeViewComponent {
-  sidebarVisible: boolean = false
+
 
 }
