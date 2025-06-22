e8b22fa13ceab196094083486dcaa1452e23afd0
unknown <pablomartinezcoder@gmail.com>
2025-06-22 12:18:34 +0200

    Refactor: Remove old components and imports

--- notebook-frontend/src/app/features/note/pages/view-note/view-note.component.ts
+++ notebook-frontend/src/app/features/note/pages/view-note/view-note.component.ts
@@ -1,17 +1,14 @@
 import {Component} from '@angular/core';
 import {CodeEditorComponent} from '../../components/editor/code-editor.component';
-import {SidebarComponent} from '../../components/sidebar/sidebar.component';
 import {Splitter} from 'primeng/splitter';
 import {ViewerComponent} from '../../components/viewer/viewer.component';
 import {NOTE_MARKDOWN} from '../../../../core/app.constants';
-import {ToolbarComponent} from '../../components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-view-note',
   imports: [
     Splitter,
     ViewerComponent,
-    ToolbarComponent,
     CodeEditorComponent
   ],
   templateUrl: './view-note.component.html'
--- notebook-frontend/src/app/features/welcome/pages/welcome-view/welcome-view.component.ts
+++ notebook-frontend/src/app/features/welcome/pages/welcome-view/welcome-view.component.ts
@@ -1,15 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
 import {PickerComponent} from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-welcome-view',
   imports: [
-    PickerComponent
-
   ],
   templateUrl: './welcome-view.component.html'
 })
 export class WelcomeViewComponent {
+
+  emoji: WritableSignal<string> = signal<string>("");
 
 
 }
