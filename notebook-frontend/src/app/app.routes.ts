1b8f8f389258a54f61c28f3684ae8128b071b79a
unknown <pablomartinezcoder@gmail.com>
2025-06-28 10:03:14 +0200

    Feat: Add create note component

--- notebook-frontend/src/app/app.routes.ts
+++ notebook-frontend/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import {Routes} from '@angular/router';
 import {ViewNoteComponent} from './features/note/pages/view-note/view-note.component';
 import {WelcomeViewComponent} from './features/welcome/pages/welcome-view/welcome-view.component';
+import {CreateNoteComponent} from './features/note/pages/create-note/create-note.component';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: '/welcome',
     pathMatch: 'full'
+  },
+  {
+    path: 'note/create',
+    component: CreateNoteComponent
   },
   {
     path: 'note/:id',
---
+++ notebook-frontend/src/app/features/note/pages/create-note/create-note.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-note',
+  imports: [],
+  templateUrl: './create-note.component.html'
+})
+export class CreateNoteComponent {
+
+}
