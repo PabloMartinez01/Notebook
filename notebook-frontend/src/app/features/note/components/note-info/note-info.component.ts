8603333b9dc5145ef5a50e6613ea3ad0431630ff
unknown <pablomartinezcoder@gmail.com>
2025-07-14 17:08:51 +0200

    Feat: Add UI icon picker

--- notebook-frontend/src/app/features/note/components/note-info/note-info.component.ts
+++ notebook-frontend/src/app/features/note/components/note-info/note-info.component.ts
@@ -1,12 +1,25 @@
-import {Component, input, InputSignal, model, ModelSignal, output, OutputEmitterRef} from '@angular/core';
+import {
+  Component, ElementRef, HostListener,
+  input,
+  InputSignal,
+  model,
+  ModelSignal,
+  output,
+  OutputEmitterRef, signal, ViewChild,
+  WritableSignal
+} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {DatePipe} from '@angular/common';
+import {PickerComponent} from '@ctrl/ngx-emoji-mart';
+import {EmojiComponent, EmojiData} from '@ctrl/ngx-emoji-mart/ngx-emoji';
 
 @Component({
   selector: 'note-info',
   imports: [
     FormsModule,
-    DatePipe
+    DatePipe,
+    PickerComponent,
+    EmojiComponent
   ],
   templateUrl: './note-info.component.html'
 })
@@ -18,6 +31,10 @@ export class NoteInfoComponent {
   title: InputSignal<string> = model.required<string>();
   date: InputSignal<string> = input.required<string>();
 
+  showIconPicker: WritableSignal<boolean> = signal<boolean>(false);
+  icon = signal(":smile:")
+
+
   save(): void {
     this.onSave.emit();
   }
@@ -26,5 +43,16 @@ export class NoteInfoComponent {
     const value: string = (event.target as HTMLInputElement).value ?? '';
     this.onChangeTitle.emit(value);
   }
+
+  onChangeIcon(event: { emoji: EmojiData; $event: PointerEvent }) {
+    this.icon.set(event.emoji.colons ?? '');
+    this.showIconPicker.set(false);
+  }
+
+  controlIconPicker(show: boolean) {
+    this.showIconPicker.set(show);
+  }
+
+
 
 }
